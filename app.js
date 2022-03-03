494bb71a5a0a1989da86681586a9ebe6e4f293b9
AnnabelleAB <sunhuaqing01@gmail.com>
2022-03-03 14:49:44 -0800

    update google callback url with heroku site

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "https://secrets-annabelle-sun.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo" //to retreive the user info not from Google+ as it will be deprecated
   },
   function(accessToken, refreshToken, profile, cb) {
